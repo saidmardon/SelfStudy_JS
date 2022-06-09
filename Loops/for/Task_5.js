b29abba6b105f2f3fa71128447b294c2af6d2db0
Saymumin <mardonov240687@gmail.com>
2022-06-10 00:40:07 +0500

    Loop for is learned

---
+++ Loops/for/Task_5.js
@@ -0,0 +1,5 @@
+for(let i=1; i<=10; i++){
+      count = 1;
+      result = count*i;
+      console.log(`${count} * ${i} = ${result}`)
+}
---
+++ Loops/for/Task_6_arrayWthFor.js
@@ -0,0 +1,4 @@
+let arr = [2, 5, 6, 9, 0];
+for(i=0; i<arr.length; i++){
+      console.log(arr[i]*2)
+}
