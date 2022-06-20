5ada3aec4c8675dc4be42fbbc87604fd0ba187ed
Saymumin <mardonov240687@gmail.com>
2022-06-20 21:32:40 +0500

    Quotes are very helpfull

---
+++ String/quotes.js
@@ -0,0 +1,11 @@
+let str1 = "Hello\n I\'m Saymumin,\n Now leraning\t Javascript"; //двойные кавычки
+let str2 = 'Hello my name is Saymumin'; //одинарные кавычках
+let str3 = `Hello my name is Saymumin`; //обратные кавычки
+console.log(str1)
+
+
+let name = "Saymumin";
+let age = 34;
+let str = `Hello my name is ${name}, I am ${age} years old`;
+console.log(str);
+//Hello my name is Saymumin, I am 34 years old
